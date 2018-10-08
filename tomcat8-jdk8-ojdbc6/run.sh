aa0d50c7a3f3587717f795bb6958a4e401c59860
Edgar Alonso <ealonso@okode.com>
2018-10-08 13:59:20 +0200

    Added sed command to comment line at manager/META-INF/config.xml

--- tomcat8-jdk8-ojdbc6/run.sh
+++ tomcat8-jdk8-ojdbc6/run.sh
@@ -24,7 +24,8 @@ echo "Setting manager max limit from 50MB to 100MB just in case"
 
 sed -i -e "s/52428800/104857600/g" /opt/tomcat8/webapps/manager/WEB-INF/web.xml
 
-sed -i -e "s/<Valve className=\"org.apache.catalina.valves.RemoteAddrValve\"\n	 allow=\"127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1\" />/<!--Valve className=\"org.apache.catalina.valves.RemoteAddrValve\"\n	 allow=\"127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1\" -->/g" /opt/tomcat8/webapps/manager/META-INF/context.xml
+sed -i -e "s/<Valve className=\"org.apache.catalina.valves.RemoteAddrValve\"/<!--Valve className=\"org.apache.catalina.valves.RemoteAddrValve\"/g" /opt/tomcat8/webapps/manager/META-INF/context.xml
+sed -i -e "allow=\"127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1\" -->/ allow=\"127\.\d+\.\d+\.\d+|::1|0:0:0:0:0:0:0:1\" /-->" /opt/tomcat8/webapps/manager/META-INF/context.xml
 
 echo "Installing additional certs"
 
